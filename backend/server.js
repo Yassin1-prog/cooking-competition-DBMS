44be388edff8ea72417c1d9b3153ccc246ba41a8
Yassin1-prog <132544868+Yassin1-prog@users.noreply.github.com>
2025-02-28 20:44:38 +0200

    frontend set up

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,6 @@ app.use("/cooks", cookRouter);
 app.use("/episodes", episodeRouter);
 app.use("/general", generalRouter);
 
-app.get("/users", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT * FROM recipe");
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Database error");
-  }
-});
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
